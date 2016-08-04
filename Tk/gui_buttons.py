1ef7252edc3b747edd4de02004da549ccfa01f5b
vanstinger <vandykfernandes@hotmail.com>
2016-08-05 02:21:10 +0400

    tkinter buttons

---
+++ Tk/gui_buttons.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+window = Tk()
+
+button1 = Button(None, text = "Click on Me!", fg = "Red")
+button1.pack()
+
+button2 = Button(None, text = "Hello!", fg = "Blue")
+button2.pack()
+
+window.mainloop()
