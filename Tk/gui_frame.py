467d66745173d32cf78f6edf9820a72adee254a6
vanstinger <vandykfernandes@hotmail.com>
2016-08-05 02:30:51 +0400

    tkinter frames UPDATED

--- Tk/gui_frame.py
+++ Tk/gui_frame.py
@@ -9,15 +9,15 @@ botFrame = Frame(window)
 botFrame.pack(side=BOTTOM)
 
 button1 = Button(topFrame, text="Click1", fg="Red")
-button1.pack()
+button1.pack(side=LEFT)
 
 button2 = Button(topFrame, text="Click2", fg="Blue")
-button2.pack()
+button2.pack(side=LEFT)
 
 button3 = Button(botFrame, text="Click3", fg="Green")
-button3.pack()
+button3.pack(side=LEFT)
 
 button4 = Button(botFrame, text="Click4", fg="Yellow")
-button4.pack()
+button4.pack(side=LEFT)
 
 window.mainloop()
