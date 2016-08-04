0009eae47f30b14e417b73bba2e0281b56d54998
vanstinger <vandykfernandes@hotmail.com>
2016-08-05 02:14:51 +0400

    Label with tkinter

---
+++ Tk/gui1.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+# Intialize tkinter
+root = Tk()
+
+# Creates a Label
+add_label = Label(root, text="Initial tkinter window screen")
+# Pack the widget into the window screen
+add_label.pack()
+
+root.mainloop()
